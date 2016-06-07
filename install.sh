5e1fca5a7c77d29d1f92e19ce8d9d56927b86326
starksm64 <starksm64@gmail.com>
2016-06-07 13:30:38 -0700

    Add an option to build the statically link version

--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+static=0
+while getopts s opt
+do
+  case $opt in
+    s) static=1 ;;
+    *) exit 1 ;;
+  esac
+done
+
 if [ ! -e Debug ]; then
     mkdir Debug
 fi
+
+if [ $static -gt 0 ]
+then
+   echo "Using static linking against BlueZ"
+   export BluezLinkType="static"
+fi
+
 cd Debug
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 cd ..
